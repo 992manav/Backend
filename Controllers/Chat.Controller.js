21dfcb3d64681b2f7a9532ba4ef69095912e1917
Kautilya03 <23ucs613@gmail.com>
2025-03-22 09:57:09 +0530

    motu

--- Controllers/Chat.Controller.js
+++ Controllers/Chat.Controller.js
@@ -4,15 +4,19 @@ import {Message} from "../Models/Message.Model.js";
 const router = express.Router();
 
 router.get("/:sender/:receiver", async (req, res) => {
-    const { sender, receiver } = req.params;
-    const messages = await Message.find({
-      $or: [
-        { sender, receiver },
-        { sender: receiver, receiver: sender },
-      ],
-    }).sort({ createdAt: 1 });
-  
-    res.json(messages);
+    try {
+        const { sender, receiver } = req.params;
+        const messages = await Message.find({
+          $or: [
+            { sender, receiver },
+            { sender: receiver, receiver: sender },
+          ],
+        }).sort({ createdAt: 1 });
+      
+        res.json(messages);
+    } catch (error) {
+        console.log(error)
+    }
 });
 
 export default router;
